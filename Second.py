dc5455a44777bed4af918d7b2ca99207603c9dfb
Ryan Moore <125109206+RyanCrf7@users.noreply.github.com>
2024-08-16 23:26:59 -0300

    Update Second.py

--- Second.py
+++ Second.py
@@ -3,3 +3,5 @@ num2 = int(input("Enter the value of the second number: "))
 sum_result = num1 + num2
 product = num1 * num2
 print(f"The sum of the numbers: {num1} and {num2} is {sum_result} and the product is {product}")
+
+exit()
